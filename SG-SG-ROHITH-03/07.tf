a4d58d3c591cf7513fb287b09cd4855d7547d236
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-07-03 15:07:25 +0530

    Delete SG-SG-ROHITH-03 directory

--- SG-SG-ROHITH-03/07.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-sg-rohith-03/07" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sg-rohith-03/07"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
